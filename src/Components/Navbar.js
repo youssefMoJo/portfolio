cf9f8f2ec38ae71e81feebe68429d5e27efcc848
youssefMoJo <youssefrafat@yahoo.com>
2023-03-11 09:37:34 -0330

    Navbar is now responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,29 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/Navbar.css";
+
 const Navbar = () => {
+  let navBarContent = ["#Home", "#About Me", "#Projects", "#Contact"];
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div>Navbar</div>
+    <header>
+      <h3> Logo </h3>
+      <nav ref={navRef}>
+        {navBarContent.map((c) => (
+          <a href="#/"> {c} </a>
+        ))}
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
 };
 
