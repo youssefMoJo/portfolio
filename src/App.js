598fde17003e39188c45f555282b7f4d0035eb91
youssefMoJo <youssefrafat@yahoo.com>
2023-03-11 08:51:17 -0330

    added all components and added some styling to the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Home />
+      <Projects />
+      <About />
+      <Contact />
     </div>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="Index">
+      <App />
+    </div>
   </React.StrictMode>
 );
 
