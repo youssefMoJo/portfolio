3f3176e3907ec1acf0511f537ddf759ded1b712f
youssefMoJo <youssefrafat@yahoo.com>
2023-03-11 16:44:41 -0330

    Finished the left side of my Home section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Components/Home";
 import Projects from "./Components/Projects";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
-import "./App.css"
+import "./App.css";
 
 const App = () => {
   return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,30 @@
+import "../Styles/Home.css";
+import { BiDownload } from "react-icons/bi";
+
 const Home = () => {
-  return <div> Home </div>;
+  return (
+    <div className="HomeContainer">
+      <div className="LeftSideContainer">
+        <div className="SideDecorationCircleShape">
+          <div className="SideDecorationLineShape"></div>
+        </div>
+
+        <div>
+          <h1 className="Hello">HELLO</h1>
+          <h2 className="MyNameSentence">
+            I Am <span className="MyName">Youssef Mohamed</span>
+          </h2>
+          <h2 className="Title">Full Stack Developer</h2>
+          <div className="ResumeButton">
+            Download Resume
+            <BiDownload className="DownloadIcon" />
+          </div>
+        </div>
+      </div>
+
+      <div>Circle shape compnent</div>
+    </div>
+  );
 };
 
 export default Home;
