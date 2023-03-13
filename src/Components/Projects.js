5e04772698babe65521f3837c20b95b6a187faaa
youssefMoJo <youssefrafat@yahoo.com>
2023-03-13 20:06:41 -0230

    intiated the Projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,13 @@
+import "../Styles/Projects.css";
+
 const Projects = () => {
-  return <div> Projects </div>;
+  return (
+    <div>
+      <h1 className="ProjectsTitle">
+        <span className="PHashtag">#</span> Projects
+      </h1>
+    </div>
+  );
 };
 
 export default Projects;
