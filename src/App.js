185eac3d602aebe644e8d71b53e8e50fd27113a7
youssefMoJo <youssefrafat@yahoo.com>
2023-04-07 00:33:15 -0230

    testing google analytics feature

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import Contact from "./Components/Contact";
 import Fly from "./Components/Fly";
 import Rights from "./Components/Rights";
 import "./App.css";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    // ReactGA.initialize("TRACKING ID");
+    ReactGA.initialize("G-KD8HMKMFZ5");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <div className="App">
       <Navbar />
