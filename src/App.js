577a1b7bb69754bb12d5a3aa6ce6378929ccb585
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-03 20:28:08 -0330

    "Implemented a check for screen width below 1490px to switch back to the old hero design."

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useRef, useState } from "react";
+
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Projects from "./Components/Projects";
@@ -9,11 +11,24 @@ import NewHome from "./Components/NewHome";
 import "./App.css";
 
 const App = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
-      <NewHome />
-      {/* <Home /> */}
+      {screenWidth >= 1490 ? <NewHome /> : <Home />}
       <Fly />
       <Projects />
       <About />
