38c1d054ac57010a88dedc318284fcc51392b6d9
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-02 22:48:33 -0330

    finished the splitter animation

--- src/Components/NewHomeSides.js/NewHomeLeftSide.js
+++ src/Components/NewHomeSides.js/NewHomeLeftSide.js
@@ -4,18 +4,31 @@ const NewHomeLeftSide = () => {
     fontFamily: "Playfair Display",
     display: "flex",
     alignItems: "center",
+    gap: "3rem",
   };
 
   const yAndMSplitterStyle = {
-    // width: "2rem",
     height: "20rem",
     borderRight: "13px solid white",
-    margin: "0 1rem",
     borderRadius: "4px",
+    animation: "expand 1s ease",
   };
 
   return (
     <div>
+      <style>
+        {`
+          @keyframes expand {
+            0% {
+              height: 0;
+            }
+           
+            100% {
+              height: 20rem;
+            }
+          }
+        `}
+      </style>
       <div style={yAndMStyle}>
         Y<span style={yAndMSplitterStyle}></span>M
       </div>
