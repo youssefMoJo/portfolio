a87b5e7a3f9d8f98ce6c7da2a801af6c009dbf05
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2023-12-02 21:57:13 -0330

    finished the styling of the "newHomeContainer"

--- src/Components/NewHome.js
+++ src/Components/NewHome.js
@@ -1,7 +1,11 @@
 const NewHome = () => {
   const newHomeContainer = {
     height: "100vh",
-    backgroundColor: "red",
+    // backgroundColor: "red",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "4rem",
   };
   return <div style={newHomeContainer}>new home</div>;
 };
