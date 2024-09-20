e536fd6b6649f2d0143850539f42da05b6e4933c
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-09-20 21:09:03 -0230

    Updated the about me section

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,21 +16,18 @@ const About = () => {
           <h1 className="AboutParagraphQuestion">/Who am i?</h1>
         </div>
         <p className="AboutParagraph">
-          Hi, I'm Youssef—a dedicated full-stack web developer and enthusiastic
-          content creator. You'll often find me crafting engaging shorts on
-          Facebook and Instagram, driven by a passion for exploring the
-          intersection of technology and its potential to enhance our daily
-          lives. Whether I'm immersed in coding or sharing insights on the
-          profound impact of technology, I thrive on deepening my understanding
-          and cultivating knowledge in every endeavor.
+          Hi, I'm Youssef—a dedicated full-stack developer and enthusiastic
+          content creator. I love crafting engaging short-form content on social
+          media, driven by a passion for exploring how technology can enhance
+          our daily lives. Whether immersed in coding or sharing insights on the
+          transformative power of tech, I thrive on expanding my knowledge and
+          contributing to meaningful projects.
         </p>
         <p className="AboutParagraph">
-          As a Train-the-Trainer Facilitator with techKNOWtutors at CSCNL since
-          January 2024, I actively create curriculum and conduct workshops,
-          empowering individuals with crucial digital skills in Newfoundland.
-          Additionally, I collaborate with nonprofits and volunteers to secure
-          the sustainability of digital skills initiatives, extending the impact
-          far beyond the scope of techKNOWtutors.
+          I'm always eager to deepen my understanding of the latest technologies
+          and find innovative ways to solve problems, whether in development or
+          digital content creation. My goal is to continuously grow and use my
+          skills to make a positive impact in the tech space.
         </p>
       </div>
     </div>
